002c717af299c5b40b224b0959ac1e61c1e0a1d4
HB-BH2005 <hadil.barzani@esi.ac.ma>
2025-04-29 12:06:39 +0100

    ...

--- routes/web.php
+++ routes/web.php
@@ -34,61 +34,58 @@ Route::get('lessons/{lesson}', [LessonController::class, 'show'])->name('lessons
 
 //========================User Routes==============================================
 
-Route::middleware(['auth', 'admin']) // <- 'admin' is the alias (string)
-    ->prefix('admin')
+
+// Admin Routes - Grouped and protected by 'auth' and 'admin' middleware
+Route::middleware(['auth', 'admin']) // Ensure only authenticated admin users can access these routes
+    ->prefix('admin') // All admin routes will be prefixed with '/admin'
     ->group(function () {
+        // Admin Home
         Route::get('/admin', fn() => view('admin.home'))->name('admin.home');
-        
-        
+
+        // Admin: Users Management
+        Route::get('users', [UserController::class, 'index'])->name('admin.users.index');
+        Route::get('users/create', [UserController::class, 'create'])->name('admin.users.create');
+        Route::post('users', [UserController::class, 'store'])->name('admin.users.store');
+        Route::delete('users/{id}', [UserController::class, 'destroy'])->name('admin.users.destroy');
+
+        // Admin: Levels Management
+        Route::get('levels', [LevelController::class, 'adminIndex'])->name('admin.levels.index');
+        Route::get('levels/create', [LevelController::class, 'create'])->name('admin.levels.create');
+        Route::post('levels', [LevelController::class, 'store'])->name('admin.levels.store');
+        Route::get('levels/{id}/edit', [LevelController::class, 'edit'])->name('admin.levels.edit');
+        Route::put('levels/{id}', [LevelController::class, 'update'])->name('admin.levels.update');
+        Route::delete('levels/{id}', [LevelController::class, 'destroy'])->name('admin.levels.destroy');
+
+        // Admin: Subjects Management
+        Route::prefix('subjects')->group(function () {
+            Route::get('/', [SubjectController::class, 'adminIndex'])->name('admin.subjects.index');
+            Route::get('/create', [SubjectController::class, 'create'])->name('admin.subjects.create');
+            Route::post('/', [SubjectController::class, 'store'])->name('admin.subjects.store');
+            Route::get('/{id}/edit', [SubjectController::class, 'edit'])->name('admin.subjects.edit');
+            Route::put('/{id}', [SubjectController::class, 'update'])->name('admin.subjects.update');
+            Route::delete('/{id}', [SubjectController::class, 'destroy'])->name('admin.subjects.destroy');
+        });
+
+        // Admin: Lessons Management
+        Route::prefix('lessons')->group(function () {
+            Route::get('/', [LessonController::class, 'index'])->name('admin.lessons.index');
+            Route::get('/create', [LessonController::class, 'create'])->name('admin.lessons.create');
+            Route::post('/', [LessonController::class, 'store'])->name('admin.lessons.store');
+            Route::get('/{id}/edit', [LessonController::class, 'edit'])->name('admin.lessons.edit');
+            Route::put('/{id}', [LessonController::class, 'update'])->name('admin.lessons.update');
+            Route::delete('/{id}', [LessonController::class, 'destroy'])->name('admin.lessons.destroy');
+        });
+
+        // Admin: Lesson Content Management
+        Route::prefix('lesson_contents')->group(function () {
+            Route::get('/', [LessonContentController::class, 'index'])->name('admin.lesson_contents.index');
+            Route::get('/create', [LessonContentController::class, 'create'])->name('admin.lesson_contents.create');
+            Route::post('/', [LessonContentController::class, 'store'])->name('admin.lesson_contents.store');
+            Route::get('/{id}/edit', [LessonContentController::class, 'edit'])->name('admin.lesson_contents.edit');
+            Route::put('/{id}', [LessonContentController::class, 'update'])->name('admin.lesson_contents.update');
+            Route::delete('/{id}', [LessonContentController::class, 'destroy'])->name('admin.lesson_contents.destroy');
+        });
+
+        // Admin: Logout
+        Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
     });
-
-
-
-// Admin Routes
-//
-
-// Admin: Users Management
-Route::get('admin/users', [UserController::class, 'index'])->name('admin.users.index');
-Route::get('admin/users/create', [UserController::class, 'create'])->name('admin.users.create');
-Route::post('admin/users', [UserController::class, 'store'])->name('admin.users.store');
-Route::delete('admin/users/{id}', [UserController::class, 'destroy'])->name('admin.users.destroy');
-
-// Admin: Levels Management
-Route::get('/admin/levels', [LevelController::class, 'adminIndex'])->name('admin.levels.index');
-Route::get('/admin/levels/create', [LevelController::class, 'create'])->name('admin.levels.create');
-Route::post('/admin/levels', [LevelController::class, 'store'])->name('admin.levels.store');
-Route::get('/admin/levels/{id}/edit', [LevelController::class, 'edit'])->name('admin.levels.edit');
-Route::put('/admin/levels/{id}', [LevelController::class, 'update'])->name('admin.levels.update');
-Route::delete('/admin/levels/{id}', [LevelController::class, 'destroy'])->name('admin.levels.destroy');
-
-// Admin: Subjects Management
-Route::prefix('admin/subjects')->group(function () {
-    Route::get('/', [SubjectController::class, 'adminIndex'])->name('admin.subjects.index');
-    Route::get('/create', [SubjectController::class, 'create'])->name('admin.subjects.create');
-    Route::post('/', [SubjectController::class, 'store'])->name('admin.subjects.store');
-    Route::get('/{id}/edit', [SubjectController::class, 'edit'])->name('admin.subjects.edit');
-    Route::put('/{id}', [SubjectController::class, 'update'])->name('admin.subjects.update');
-    Route::delete('/{id}', [SubjectController::class, 'destroy'])->name('admin.subjects.destroy');
-});
-
-// Admin: Lessons Management
-Route::prefix('admin/lessons')->group(function () {
-    Route::get('/', [LessonController::class, 'index'])->name('admin.lessons.index');
-    Route::get('/create', [LessonController::class, 'create'])->name('admin.lessons.create');
-    Route::post('/', [LessonController::class, 'store'])->name('admin.lessons.store');
-    Route::get('/{id}/edit', [LessonController::class, 'edit'])->name('admin.lessons.edit');
-    Route::put('/{id}', [LessonController::class, 'update'])->name('admin.lessons.update');
-    Route::delete('/{id}', [LessonController::class, 'destroy'])->name('admin.lessons.destroy');
-});
-//Admin: Lesson Content Management
-Route::prefix('admin/lesson_contents')->group(function () {
-    Route::get('/', [LessonContentController::class, 'index'])->name('admin.lesson_contents.index');
-    Route::get('/create', [LessonContentController::class, 'create'])->name('admin.lesson_contents.create');
-    Route::post('/', [LessonContentController::class, 'store'])->name('admin.lesson_contents.store');
-    Route::get('/{id}/edit', [LessonContentController::class, 'edit'])->name('admin.lesson_contents.edit');
-    Route::put('/{id}', [LessonContentController::class, 'update'])->name('admin.lesson_contents.update');
-    Route::delete('/{id}', [LessonContentController::class, 'destroy'])->name('admin.lesson_contents.destroy');
-});
-
-// Logout
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
